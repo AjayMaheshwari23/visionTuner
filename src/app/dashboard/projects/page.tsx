cf4349858639a1d04defa2944004f78174e59fed
Ajay Maheshwari <lci2021023@iiitl.ac.in>
2024-03-02 00:05:05 +0530

    LOGIn-LOgout added

---
+++ src/app/dashboard/projects/page.tsx
@@ -0,0 +1,14 @@
+import ProjectCard from "../../../components/cards/ProjectCard";
+import ModalComp from "../../../components/modal/modal";
+
+const ProjectPage = () => {
+
+  return (
+    <>
+      <ProjectCard />
+      <ModalComp />
+    </>
+  );
+};
+
+export default ProjectPage;
