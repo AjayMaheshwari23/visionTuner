34683f5836be24b82eac863b1b34f677df1f846f
Ajay Maheshwari <lci2021023@iiitl.ac.in>
2024-03-03 16:34:34 +0530

    Updated Modal + Dummy Annotation + api for new Project

---
+++ src/components/annotateTool/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import WIP from '../../../public/assets/images/WIP.png'
+
+const AnnotateTool = () =>  {
+    return (
+        <>
+        <Image alt="WORK in Progress" src={WIP}/>
+        </>
+    )
+}
+
+export default AnnotateTool;
