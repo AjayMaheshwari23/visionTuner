f3b1c728c09c417e3da90ccf5633940a636607fa
Narendra-LCI32 <lci2021032@iiitl.ac.in>
2024-03-10 01:25:36 +0530

    Added Settings Page

--- src/app/dashboard/settings/page.tsx
+++ src/app/dashboard/settings/page.tsx
@@ -1,7 +1,39 @@
+"use client"
+import { Switch } from "antd";
+import "../../../styles/Settings.css"
+
 export default function settings(){
+  const onChange = (checked) => {
+    console.log(`switch to ${checked}`);
+  };
     return (
       <>
-        <h1>Let's set things!</h1>
+        <div className="container">
+  <div className="buttonsContainer">
+  <div className="button">
+      <p className="buttonText">Dark Theme</p>
+      <Switch defaultChecked onChange={onChange} />
+    </div>
+    <div className="button">
+      <div className="buttonText">Enable Notifications</div>
+      <Switch defaultChecked onChange={onChange} />
+    </div>
+  </div>
+  <div className="buttonContainer-1">
+  <div className="button">
+      <div className="buttonText">Features-1</div>
+      <Switch defaultChecked onChange={onChange} />
+    </div>
+    <div className="button">
+      <div className="buttonText">Features-2</div>
+      <Switch defaultChecked onChange={onChange} />
+    </div>
+  </div>
+  <div className="proContainer">
+    <div className="proButtons">Delete a Project</div>
+    <div className="proButtons">Upgrade to Pro</div>
+  </div>
+</div>
       </>
     );
 }
