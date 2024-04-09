f7bc1bb1027e71906c8c1d48c0563893f3271a1f
Ajay Maheshwari <lci2021023@iiitl.ac.in>
2024-04-09 22:52:01 +0530

    Fix try 2.0

--- src/app/dashboard/projects/[projectId]/page.tsx
+++ src/app/dashboard/projects/[projectId]/page.tsx
@@ -1,19 +1,19 @@
-'use client'
+"use client";
 import { Button } from "antd";
 import { useAppContext } from "@/contexts/AppContext";
 import { Project } from "@/app/models/user";
 import { useRouter } from "next/navigation";
-import ProjectPageIndividual from "@/components/Pages/Projects";
+import ProjectPageIndividual from "@/components/Pages/ProjectPageIndividual";
 
 export default function Page({ params }: { params: { projectId: string } }) {
-
   const router = useRouter();
-  const {state} = useAppContext();
-  const CurProject : Project | undefined = state.user?.projects[params.projectId as unknown as number];
+  const { state } = useAppContext();
+  const CurProject: Project | undefined =
+    state.user?.projects[params.projectId as unknown as number];
   return (
     <>
       <Button onClick={() => router.back()}> Go Back</Button>
-      { CurProject && <ProjectPageIndividual project={CurProject} />}
+      {CurProject && <ProjectPageIndividual project={CurProject} />}
     </>
   );
 }
