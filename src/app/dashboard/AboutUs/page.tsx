e5467c49f99f6dd761f76cfc5a1f29d591489219
Mahekjain2706 <lci2021028@iiitl.ac.in>
2024-04-21 17:24:29 +0530

    error resolve

--- src/app/dashboard/AboutUs/page.tsx
+++ src/app/dashboard/AboutUs/page.tsx
@@ -1,9 +1,12 @@
+import React from 'react';
+
 const AboutUs = () => {
-    return (
-      <>
-        <h1>AboutUs</h1>
-      </>
-    );
+  return (
+    <>
+      
+    </>
+
+  );
 }
 
 export default AboutUs;
--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -1,32 +1,33 @@
-"use client";
-import '../../../../src/styles/profile.css';
-import { useAppContext } from "../../../contexts/AppContext";
-import { Card,Col,Row,Input,Button,Image,Upload } from 'antd';
+"use client"
+import React, { useState } from 'react';
+import { Upload, Input, Button, Image, Row, Col } from 'antd';
 import { PlusOutlined } from '@ant-design/icons';
-import React , { useState } from 'react';
+import { UploadFile } from 'antd/lib/upload/interface'; // Import UploadFile type
+import { useAppContext } from "../../../contexts/AppContext";
+import '../../../../src/styles/profile.css';
 
-const getBase64 = (file : any) =>
-  new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => resolve(reader.result);
-    reader.onerror = (error) => reject(error);
-});
-const Profile = () => {
-  const { state, setState } = useAppContext();
+interface FileItem extends UploadFile<any> {
+  // You can add additional properties specific to your application's file items here
+}
+
+const Profile: React.FC = () => {
+  const { state } = useAppContext();
   const [previewOpen, setPreviewOpen] = useState(false);
-  const [previewImage, setPreviewImage] = useState('');
-  const [fileList, setFileList] = useState([
-    
-  ]);
-  const handlePreview = async (file : any) => {
+  const [previewImage, setPreviewImage] = useState<string>('');
+  const [fileList, setFileList] = useState<FileItem[]>([]);
+
+  const handlePreview = async (file: FileItem) => {
     if (!file.url && !file.preview) {
-      file.preview = await getBase64(file.originFileObj);
+      file.preview = await getBase64(file.originFileObj!);
     }
-    setPreviewImage(file.url || file.preview);
+    setPreviewImage(file.url || file.preview || '');
     setPreviewOpen(true);
   };
-  const handleChange = ({ fileList: newFileList}) => setFileList(newFileList);
+
+  const handleChange = ({ fileList: newFileList }: { fileList: FileItem[] }) => {
+    setFileList(newFileList);
+  };
+
   const uploadButton = (
     <button
       style={{
@@ -45,41 +46,49 @@ const Profile = () => {
       </div>
     </button>
   );
-  return(
+
+  const getBase64 = (file: File | Blob) =>
+    new Promise<string>((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result as string);
+      reader.onerror = (error) => reject(error);
+    });
+
+  return (
     <div className="container">
       <div className='card'>
-      <Upload
-        action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
-        listType="picture-circle"
-        fileList={fileList}
-        onPreview={handlePreview}
-        onChange={handleChange}
-      >
-        {fileList.length >= 8 ? null : uploadButton}
-      </Upload>
-      {previewImage && (
-        <Image
-          wrapperStyle={{
-            display: 'none',
-          }}
-          preview={{
-            visible: previewOpen,
-            onVisibleChange: (visible) => setPreviewOpen(visible),
-            afterOpenChange: (visible) => !visible && setPreviewImage(''),
-          }}
-          src={previewImage}
-        />
-      )}
+        <Upload
+          action="https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload"
+          listType="picture-circle"
+          fileList={fileList}
+          onPreview={handlePreview}
+          onChange={handleChange}
+        >
+          {fileList.length >= 8 ? null : uploadButton}
+        </Upload>
+        {previewImage && (
+          <Image
+            wrapperStyle={{
+              display: 'none',
+            }}
+            preview={{
+              visible: previewOpen,
+              onVisibleChange: (visible) => setPreviewOpen(visible),
+            }}
+            src={previewImage}
+          />
+        )}
         <Row>
           <Col span={12} className='col'>{state.user?.username}</Col>
-          <Col span={12} className='col'><Input.Password placeholder="Old Password"/></Col>
+          <Col span={12} className='col'><Input.Password placeholder="Old Password" /></Col>
         </Row>
-        <br/>
+        <br />
         <Row>
           <Col span={12} className='col'>{state.user?.username}</Col>
-          <Col span={12} className='col'><Input.Password placeholder="New Password"/></Col>
+          <Col span={12} className='col'><Input.Password placeholder="New Password" /></Col>
         </Row>
-        <br/>
+        <br />
         <Row>
           <Col span={12} className='col'>{state.user?.email}</Col>
           <Col span={12} className='col'><Button type='primary'>Upload it !!</Button></Col>
@@ -88,5 +97,5 @@ const Profile = () => {
     </div>
   );
 };
-export default Profile;
 
+export default Profile;
